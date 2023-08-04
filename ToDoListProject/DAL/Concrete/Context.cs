f88e2fec98d5b3ae4a37b92a56100a6166b49bae
Furkan UĞUR <furkanugur64@gmail.com>
2023-08-04 14:32:27 +0300

    merge

---
+++ ToDoListProject/DAL/Concrete/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoListProject.DAL.Entities;
+
+namespace ToDoListProject.DAL.Concrete
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=FRKN\\SQLEXPRESS;initial catalog=ToDoList;integrated Security=True");
+        }
+
+        public DbSet<Event> Events { get; set; }
+       
+    }
+}
